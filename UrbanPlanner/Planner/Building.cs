0628371fd6dca4845d3ffc3f19b040212ed7ecc9
Klay Thacker <klaythacker11@gmail.com>
2022-01-11 21:38:23 -0600

    Add project files.

---
+++ UrbanPlanner/Planner/Building.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner.Planner
+{
+    class Building
+    {
+        // overloading in c sharp is when you have the same name on multiple methouds but each on has it's own signature(paramaters) ex of signature = Building(string address)
+        // constructors
+        public Building()
+        {
+
+        }
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+
+        //fields
+        private string _designer = "Klay T";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+
+        // properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get => Width * Depth * (3 * Stories);
+        }
+
+        // methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchased(string name)
+        {
+            _owner = name;
+        }
+        public void PrintToConsole()
+        {
+            // @ - string verbatim - will take every keystroke literally including new lines (Enter key)
+            string output = $@"
+{_address}--------------
+Designed by {_designer}
+Constructed on {_dateConstructed}
+Owned by {_owner}
+{Volume} cubic meters of space";
+            Console.WriteLine(output);
+        }
+    }
+}
+
+
+/*public Building()
+{
+    _address = "123 Address Ave";
+}
+
+public Construct()
+{
+    _dateConstructed = DateTime.Now;
+}
+
+public Purchase()
+{
+    _owner = "Daryl";
+}
+
+private string _designer;
+
+private DateTime _dateConstructed;
+
+private string _address;
+
+private string _owner;
+
+public int Stories { get; set; }
+public double Width { get; set; }
+public double Depth { get; set; }
+private double bVolume = double.Empty;
+public double Volume
+{
+    get
+    {
+        bVolume = Math
+    }
+}*/
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UrbanPlanner.Planner;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate - use the 'new' keyword to create an isntance of an object
+            // initialize - give an instance an initial value
+            Building oneTwo = new Building("12 Address Ave")
+            {
+                Width = 5,
+                Stories = 2,
+                Depth = 10
+
+            };
+            Building threeFour = new Building("34 Address Ave")
+            {
+                Width = 7,
+                Depth = 4,
+                Stories = 3
+            };
+            Building fiveSix = new Building("56 Address Ave")
+            {
+                Width = 6,
+                Depth = 15,
+                Stories = 1
+            };
+            Building sevenEight = new Building("78 Address Ave")
+            {
+                Width = 7,
+                Depth = 4,
+                Stories = 3
+            };
+
+            oneTwo.Construct();
+            threeFour.Construct();
+            fiveSix.Construct();
+            sevenEight.Construct();
+            oneTwo.Purchased("Shelly");
+            threeFour.Purchased("Jim");
+            fiveSix.Purchased("Kim");
+            sevenEight.Purchased("Katy");
+
+            oneTwo.PrintToConsole();
+            threeFour.PrintToConsole();
+            fiveSix.PrintToConsole();
+            sevenEight.PrintToConsole();
+
+            Console.WriteLine("Thank you for your purchases!");
+        }
+    }
+}
